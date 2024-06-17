d121f459f3d0e4fd57e49210a41d6d40ac3a22c0
Luzangela Paz <lpazbeta@gmail.com>
2024-06-17 16:27:31 -0500

    ultima actualizacion

--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -1,21 +1,30 @@
 import "./Equipo.css"
-import Colaborador from "../Colaborador"
+import Colaborador from "../Colaborador/index"
+import hexToRgba from 'hex-to-rgba';
 
 const Equipo = (props) => {
     //Desctructuración
-    const{ colorPrimario, colorSecundario, titulo } = props.datos
-    const { colaboradores } = props
+    const{ colorPrimario, colorSecundario, titulo, id } = props.datos   
+    const { colaboradores, eliminarColaborador, actualizarColor, like } = props
         const obj = {
-        backgroundColor: colorSecundario
+        backgroundColor: hexToRgba(colorPrimario, 0.6)
         }
-     console.log(colaboradores.lenght > 0)
 
     const estiloTitulo = {borderColor: colorPrimario }
 
     return <>
         { 
-            colaboradores.lenght > 0 &&
+            colaboradores.length > 0 &&
             <section className="equipo" style={obj} >
+                <input 
+                    type='color'
+                    className="input-color"
+                    value={colorPrimario}
+                    onChange={(evento) => {
+                        actualizarColor(evento.target.value, id)
+                    }}
+                
+                />  
                 <h3 style={estiloTitulo} >{titulo}</h3>
                 <div className="colaboradores" >
                     {
@@ -23,6 +32,8 @@ const Equipo = (props) => {
                         datos={colaborador} 
                         key={index}
                         colorPrimario={colorPrimario}
+                        eliminarColaborador={eliminarColaborador}
+                        like={like}                        
                         />)
                     }
                 </div>
